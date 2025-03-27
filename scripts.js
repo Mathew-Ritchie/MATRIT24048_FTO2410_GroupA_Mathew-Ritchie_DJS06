64c93ea465fe5ae5e90ab0bb83d7edba2accecf3
Mathew Ritchie <ritchmat89@gmail.com>
2025-03-27 10:18:17 +0200

    1 and 2 complete.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const names = ["Ashwin", "Sibongile", "Jan-Hendrik", "Sifso", "Shailen", "Frikkie"];
 
 // A list of products with prices:
 const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
+// Number 1 //
+
+provinces.forEach((item) => console.log(item));
+names.forEach((item) => console.log(item));
+names.forEach((name, index) => console.log(`${name}(${provinces[index]})`));
+
+// Number 2 //
+
+const upperCaseProvinces = provinces.map((province) => province.toUpperCase());
+console.log(upperCaseProvinces);
