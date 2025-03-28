53b039294e2e03f255c13710f63bbcc353ce489c
Mathew Ritchie <ritchmat89@gmail.com>
2025-03-28 20:42:33 +0200

    adding comments

--- scripts.js
+++ scripts.js
@@ -23,43 +23,45 @@ const products = [
 
 // Number 1 //
 
-provinces.forEach((item) => console.log(item));
-names.forEach((item) => console.log(item));
-names.forEach((name, index) => console.log(`${name}(${provinces[index]})`));
+provinces.forEach((item) => console.log(item)); //iterating through provices array and logging each name.
+names.forEach((item) => console.log(item)); //iterating through names array and logging each name.
+names.forEach((name, index) => console.log(`${name}(${provinces[index]})`)); //iterating through names array with each names index. logging the name and using the index to log the provice with the same indext to the name.
 
 // Number 2 //
 
-const upperCaseProvinces = provinces.map((province) => province.toUpperCase());
-console.log(upperCaseProvinces);
+const upperCaseProvinces = provinces.map((province) => province.toUpperCase()); //uses map() to make a new array of provices and toUpperCase() to convert each provice to all caps.
+console.log(upperCaseProvinces); //logs new array of capitalised provises.
 
 // Number 3 //
 
-const nameLength = names.map((name) => name.length);
-console.log(nameLength);
+const nameLength = names.map((name) => name.length); // uses map() to create a new array and length() to convert the names to the number of letters
+console.log(nameLength); // logs the new array
 
 // Number 4 //
 
-const provincesSortAlph = provinces.sort();
+const provincesSortAlph = provinces.sort(); // uses .sort() to sort the provices into aphabetical order.
 console.log(provincesSortAlph);
 
 // Number 5 //
 
-const provincesFilterCape = provinces.filter((province) => !province.includes("Cape"));
-console.log(provincesFilterCape.length);
+const provincesFilterCape = provinces.filter((province) => !province.includes("Cape")); // uses filter() to create a new array, exclude (!provice)  any provice that includes "Cape" in its name.
+console.log(provincesFilterCape.length); //logs the number of provices in the new array
 
 // Number 6 //
 
 const namesWithS = names.map((name) => {
-  return name.split("").some((char) => char === "S");
+  // .map() creates a new array.
+  return name.split("").some((char) => char === "S" || char === "s"); // split() turns each name into an array of letters, some() itterates over these letter arrays and sees wich names contain either a capital "S" or (||) a lowercase "s"
 });
-console.log(namesWithS);
+console.log(namesWithS); //logs the new array that will consist of booleans, true if containing an s, false if there is no s.
 
 // Number 7 //
 
 const nameProvinceObject = names.reduce((acc, name, index) => {
-  acc[name] = provinces[index];
-  return acc;
-}, {});
+  //using reduce() on the names array passing the accumulator, name, and index
+  acc[name] = provinces[index]; //the accumulator which is the name is equal to the provice with the same index.
+  return acc; // return the accumulator
+}, {}); // intial accumulator is an empty object {}.
 
 console.log(nameProvinceObject);
 
@@ -67,9 +69,9 @@ console.log(nameProvinceObject);
 //all in one console.log
 console.log(
   // Number 1
-  products.map((item) => item.product),
+  products.map((item) => item.product), // create a new array using the map() method and targetting only the product in the products array.
   // Number 2
-  products.filter((item) => item.product.length <= 5),
+  products.filter((item) => item.product.length <= 5), //using the filter() function and .lenth to filter out the products with more than 5 characters.
   // Number 3
   products
     .filter((item) => item.price !== "" && !isNaN(Number(item.price))) // filter out empty strings && if not a number make a number
